bbe2322a2727fd4fffe68dd05e278434896ebe91
FeifanXHome <feifan.home@gmail.com>
2024-06-23 09:58:37 -0700

    6. Create Aura Character Blueprint
    add Weapon SkeletalMeshComponent for AuraCharacterBase

--- Source/Aura/Private/Character/AuraCharacterBase.cpp
+++ Source/Aura/Private/Character/AuraCharacterBase.cpp
@@ -9,6 +9,13 @@ AAuraCharacterBase::AAuraCharacterBase()
  	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = false;
 
+	// FName   -> "a regular string literal"
+	// FString -> TEXT("a regular string literal")
+	Weapon = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("Weaponn"));
+	// Weapon->SetupAttachment(GetRootComponent());
+	// FAttachmentTransformRules TransformRules(EAttachmentRule::SnapToTarget, true);
+	// Weapon->AttachToComponent(InParent, TransformRules, InSocketName);
+	Weapon->SetupAttachment(GetMesh(), FName("WeaponHandSocket"));
 }
 
 // Called when the game starts or when spawned
