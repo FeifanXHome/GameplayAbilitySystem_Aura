2b0a09402248b087731efdd7ac38162e6d697534
FeifanXHome <feifan.home@gmail.com>
2024-07-12 18:44:09 -0700

    [Test] How to associate AttributeSet's Member Variables with the modifies of GameplayEffect(BP)
    It simply creates a c++ class based on UAttributeSet, and adds a member variable FGameplayAttributeData with UPROPERTY to this class.
    The member variable will appear in the options of modifier's attribute in GameplayEffect(BP)

---
+++ Source/Aura/Public/AbilitySystem/XXXAttributeSet.h
@@ -0,0 +1,21 @@
+// Copyright XXX
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AttributeSet.h"
+#include "XXXAttributeSet.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AURA_API UXXXAttributeSet : public UAttributeSet
+{
+	GENERATED_BODY()
+	
+	FGameplayAttributeData XXXHealth;
+
+	UPROPERTY()
+	FGameplayAttributeData XXXMana;
+};
