30bf9c097a1c8bec80f4484b0281874602391363
FeifanXHome <feifan.home@gmail.com>
2024-08-02 15:31:31 -0700

    87. Aura Asset Manager (Set Project's Asset Manager Class)

--- Source/Aura/Private/AbilitySystem/AuraAbilitySystemComponent.cpp
+++ Source/Aura/Private/AbilitySystem/AuraAbilitySystemComponent.cpp
@@ -2,10 +2,16 @@
 
 
 #include "AbilitySystem/AuraAbilitySystemComponent.h"
+#include "AuraGameplayTags.h"
 
 void UAuraAbilitySystemComponent::AbilityActorInfoSet()
 {
 	OnGameplayEffectAppliedDelegateToSelf.AddUObject(this, &UAuraAbilitySystemComponent::EffectApplied);
+
+	const FAuraGameplayTags& GameplayTags = FAuraGameplayTags::Get();
+	const FString& TagToString = GameplayTags.Attributes_Secondary_Armor.ToString();
+	const FString& DebugMessage = FString::Printf(TEXT("Tag: %s"), *TagToString);
+	GEngine->AddOnScreenDebugMessage(-1, 10.f, FColor::Orange, DebugMessage);
 }
 
 void UAuraAbilitySystemComponent::EffectApplied(
