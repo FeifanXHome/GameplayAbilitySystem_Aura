7fa94e11e03685e20e2e369299e4327e789dd317
FeifanXHome <feifan.home@gmail.com>
2024-08-03 08:16:13 -0700

    88. Native Gameplay Tags

--- Source/Aura/Private/AbilitySystem/AuraAbilitySystemComponent.cpp
+++ Source/Aura/Private/AbilitySystem/AuraAbilitySystemComponent.cpp
@@ -2,16 +2,10 @@
 
 
 #include "AbilitySystem/AuraAbilitySystemComponent.h"
-#include "AuraGameplayTags.h"
 
 void UAuraAbilitySystemComponent::AbilityActorInfoSet()
 {
 	OnGameplayEffectAppliedDelegateToSelf.AddUObject(this, &UAuraAbilitySystemComponent::EffectApplied);
-
-	const FAuraGameplayTags& GameplayTags = FAuraGameplayTags::Get();
-	const FString& TagToString = GameplayTags.Attributes_Secondary_Armor.ToString();
-	const FString& DebugMessage = FString::Printf(TEXT("Tag: %s"), *TagToString);
-	GEngine->AddOnScreenDebugMessage(-1, 10.f, FColor::Orange, DebugMessage);
 }
 
 void UAuraAbilitySystemComponent::EffectApplied(
