83a90f2ac5c36d239d600c616292f07c22d4e196
FeifanXHome <feifan.home@gmail.com>
2024-08-22 11:58:20 -0700

    118. Receiving Target Data

--- Source/Aura/Private/AbilitySystem/AbilityTasks/TargetDataUnderMouse.cpp
+++ Source/Aura/Private/AbilitySystem/AbilityTasks/TargetDataUnderMouse.cpp
@@ -19,10 +19,19 @@ void UTargetDataUnderMouse::Activate()
 	}
 	else
 	{
-		//TODO: We are on the server, so listen for target data.
-	}
+		const FGameplayAbilitySpecHandle SpecHandler = GetAbilitySpecHandle();
+		const FPredictionKey ActivationPredictionKey = GetActivationPredictionKey();
 
-	
+		AbilitySystemComponent
+			->AbilityTargetDataSetDelegate(SpecHandler, ActivationPredictionKey)
+			.AddUObject(this, &UTargetDataUnderMouse::OnTargetDataReplicatedCallback);
+
+		const bool bCalledDelegate = AbilitySystemComponent->CallReplicatedTargetDataDelegatesIfSet(SpecHandler, ActivationPredictionKey);
+		if (! bCalledDelegate)
+		{
+			SetWaitingOnRemotePlayerData();
+		}
+	}
 }
 
 void UTargetDataUnderMouse::SendMouseCursorData()
@@ -46,6 +55,16 @@ void UTargetDataUnderMouse::SendMouseCursorData()
 		DataHandle,
 		FGameplayTag(),
 		AbilitySystemComponent->ScopedPredictionKey);
+
+	if (ShouldBroadcastAbilityTaskDelegates())
+	{
+		ValidData.Broadcast(DataHandle);
+	}
+}
+
+void UTargetDataUnderMouse::OnTargetDataReplicatedCallback(const FGameplayAbilityTargetDataHandle& DataHandle, FGameplayTag ActivationTag)
+{
+	AbilitySystemComponent->ConsumeClientReplicatedTargetData(GetAbilitySpecHandle(), GetActivationPredictionKey());
 
 	if (ShouldBroadcastAbilityTaskDelegates())
 	{
