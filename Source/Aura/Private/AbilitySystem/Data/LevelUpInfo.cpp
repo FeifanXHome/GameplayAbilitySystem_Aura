f47d77c205d8c25747c817f92eb5e25970e48981
FeifanXHome <feifan.home@gmail.com>
2024-11-29 18:09:37 -0800

    250. Level Up Info Data Asset

---
+++ Source/Aura/Private/AbilitySystem/Data/LevelUpInfo.cpp
@@ -0,0 +1,29 @@
+// Copyright XXX
+
+
+#include "AbilitySystem/Data/LevelUpInfo.h"
+#include "Aura/AuraLogChannels.h"
+
+int32 ULevelUpInfo::FindLevelForXP(int32 XP)
+{
+	int32 Leve = 1;
+
+	bool bSearching = true;
+	while (bSearching)
+	{
+		// LevelUpInformation[1] = Level 1 Information
+		// LevelUpInformation[2] = Level 2 Information
+		if (LevelUpInformation.Num() - 1 <= Leve) return Leve;
+
+		if (XP >= LevelUpInformation[Leve].LevelUpRequirement)
+		{
+			++Leve;
+		}
+		else
+		{
+			bSearching = false;
+		}
+	}
+
+	return Leve;
+}
