204183ef62df1b97e5fbcf6b252f4a21e3622404
FeifanXHome <feifan.home@gmail.com>
2025-01-13 16:17:20 -0800

    316. FireBolt Projectile Spread

--- Source/Aura/Private/AbilitySystem/Abilities/AuraFireBolt.cpp
+++ Source/Aura/Private/AbilitySystem/Abilities/AuraFireBolt.cpp
@@ -2,6 +2,8 @@
 
 
 #include "AbilitySystem/Abilities/AuraFireBolt.h"
+#include "Interaction/CombatInterface.h"
+#include "Kismet/KismetSystemLibrary.h"
 
 FString UAuraFireBolt::GetDescription(int32 Level)
 {
@@ -94,5 +96,44 @@ FString UAuraFireBolt::GetNextLevelDescription(int32 Level)
 		Cooldown,
 		FMath::Min(Level, NumProjectiles),
 		ScaledDamage);
+}
+
+void UAuraFireBolt::SpawnProjectiles(const FVector ProjectileTargetLocation, const FGameplayTag SocketTag, bool bOverridePitch, float PitchOverride, AActor* HomingTaget, bool IsShowDebugShapes)
+{
+	AActor* AvatarActor = GetAvatarActorFromActorInfo();
+	const bool bIsServer = AvatarActor->HasAuthority();
+	if (!bIsServer) return;
+
+	check(AvatarActor->Implements<UCombatInterface>());
+	check(AvatarActor->GetClass()->ImplementsInterface(UCombatInterface::StaticClass()));
+	const FVector SocketLocation = ICombatInterface::Execute_GetCombatSocketLocation(AvatarActor, SocketTag);
+
+	FRotator Rotation = (ProjectileTargetLocation - SocketLocation).Rotation();
+	Rotation.Pitch = bOverridePitch ? PitchOverride : 0.f;
+	const FVector Forward = Rotation.Vector();
+
+	if (IsShowDebugShapes) UKismetSystemLibrary::DrawDebugArrow(AvatarActor, SocketLocation, SocketLocation + Forward * 80.f, 18, FLinearColor::White, 13.f, 1);
+
+	//NumProjectiles = FMath::Min(MaxNumProjectiles, GetAbilityLevel());
+	//if (NumProjectiles == 1)
+	//{
+	//	SpawnProjectile(ProjectileTargetLocation, SocketTag, bOverridePitch, PitchOverride);
+	//	return;
+	//}
+
+	check(NumProjectiles > 0);
+	const float DeltaSpread = NumProjectiles == 0 ? 0 : ProjectileSpread / NumProjectiles;
+	const FVector LeftOfSpread_ = Forward.RotateAngleAxis(-ProjectileSpread / 2.f, FVector::UpVector);
+
+	for (int32 i = 0; i < NumProjectiles; i++)
+	{
+		const float Angle = DeltaSpread * (i + 0.5f);
+		const FVector Direction = LeftOfSpread_.RotateAngleAxis(Angle, FVector::UpVector);
+
+		if (IsShowDebugShapes)
+		{
+			UKismetSystemLibrary::DrawDebugArrow(AvatarActor, SocketLocation, SocketLocation + Direction * 100.f, 4, FLinearColor::MakeRandomColor(), 10.f);
+		}
+	}
 }
 
