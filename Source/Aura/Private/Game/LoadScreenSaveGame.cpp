e17c10459c29a4ce577c0d05d77ff075d97ec139
FeifanXHome <feifan.home@gmail.com>
2025-03-18 14:57:56 -0700

    407. Data Structures for Saving Data

--- Source/Aura/Private/Game/LoadScreenSaveGame.cpp
+++ Source/Aura/Private/Game/LoadScreenSaveGame.cpp
@@ -3,3 +3,33 @@
 
 #include "Game/LoadScreenSaveGame.h"
 
+bool ULoadScreenSaveGame::GetSavedMapWithMapName(const FString& InMapName, FSavedMap& OutMap)
+{
+	if (!InMapName.IsEmpty()) return false;
+
+	for (const FSavedMap& Map : SavedMaps)
+	{
+		if (Map.MapAssetName == InMapName)
+		{
+            OutMap = Map;
+            return true;
+		}
+	}
+
+    return false;
+}
+
+FSavedMap ULoadScreenSaveGame::GetSavedMapWithMapName(const FString& InMapName)
+{
+	FSavedMap Map;
+	const bool IsHasMap = GetSavedMapWithMapName(InMapName, Map);
+    return Map;
+}
+
+bool ULoadScreenSaveGame::HasMap(const FString& InMapName)
+{
+	FSavedMap Map;
+	const bool IsHasMap = GetSavedMapWithMapName(InMapName, Map);
+	return IsHasMap;
+}
+
